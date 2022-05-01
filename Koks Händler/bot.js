c242a3c46997c642dc88b4b2c26289c8bbb12500
Long1351 <kimerly370@gmail.com>
2022-05-01 14:11:55 +0200

    Code Änderung

--- Koks Händler/bot.js
+++ Koks Händler/bot.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 (async()=>{
                 let process = require('process');
                 process.on('uncaughtException', function (err) {
@@ -33,6 +34,28 @@ fire:null,
                         }
                     };
                     s4d.client = new s4d.Discord.Client({
+=======
+
+                (async()=>{
+                const token = require("./token.json")
+                const Discord = require("discord.js");
+                const Database = require("easy-json-database");
+                const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
+                const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+                const s4d = {
+                    Discord,
+                    database: new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/db.json`),
+                    joiningMember:null,
+                    reply:null,
+                    tokenInvalid:false,
+                    tokenError: null,
+                    checkMessageExists() {
+                        if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+                        if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+                    }
+                };
+                s4d.client = new s4d.Discord.Client({
+>>>>>>> Stashed changes
                     intents: [Object.values(s4d.Discord.Intents.FLAGS).reduce((acc, p) => acc | p, 0)],
                     partials: ["REACTION", "CHANNEL"]
                     });
@@ -53,6 +76,7 @@ function mathRandomInt(a, b) {
 }
 
 
+<<<<<<< Updated upstream
 await s4d.client.login('OTY1NzI4MDQ3MzM0NTU1NzI5.Yl3aVA.Q4abON_S2MmDNoAWW00rWZYnJFI').catch((e) => {
         s4d.tokenInvalid = true;
         s4d.tokenError = e;
@@ -62,6 +86,9 @@ await s4d.client.login('OTY1NzI4MDQ3MzM0NTU1NzI5.Yl3aVA.Q4abON_S2MmDNoAWW00rWZYn
             throw new Error("Intents are not turned on!")
         }
     });
+=======
+await s4d.client.login(token).catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+>>>>>>> Stashed changes
 
 s4d.client.on('messageCreate', async (s4dmessage) => {
   if ((s4dmessage.content) == ' "mülltonnensuch') {
